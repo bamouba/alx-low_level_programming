4b170d7c28fe84b77eaefb2da29bfced0a2f8f83
bamouba <bamouba2020@gmail.com>
2023-06-13 16:22:42 -0700

    README.md

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,8 +5,8 @@
  */
 void print_alphabet_x10 (void){
 	int ch;
-
-	for (ch = 'a' ; ch <= 'z' ; ++ch){
+	int i;
+	for (i = 0 ; i < 10 ; i++){
 	for (ch = 'a' ; ch <= 'z' ; ++ch){
 	_putchar(ch);
 	if (ch == 'z'){
