06fd83a6589f8b6b008136974a70c65ac5451662
bamouba <bamouba2020@gmail.com>
2023-06-13 16:40:56 -0700

    README.md

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,15 @@
  * we gonna print the alphabet 10 time in a row
  *
  */
-void print_alphabet_x10 (void){
+void print_alphabet_x10(void){
 	int ch;
+
 	int i;
-	for (i = 0 ; i < 10 ; i++){
-	for (ch = 'a' ; ch <= 'z' ; ++ch){
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+	for (ch = 'a' ; ch <= 'z' ; ++ch)
+	{
 	_putchar(ch);
 	}
 	_putchar('\n');
