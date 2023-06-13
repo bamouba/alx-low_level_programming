ae66c939d864ab89ab71263fff31e1ffbf609b58
bamouba <bamouba2020@gmail.com>
2023-06-13 16:32:22 -0700

    README.md

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,8 @@ void print_alphabet_x10 (void){
 	for (i = 0 ; i < 10 ; i++){
 	for (ch = 'a' ; ch <= 'z' ; ++ch){
 	_putchar(ch);
-	if (ch == 'z'){
-		_putchar('/n');
 	}
-	}
+	_putchar('\n');
 	}
 
 }
